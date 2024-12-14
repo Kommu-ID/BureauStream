4a322cdc08119c6a3136f41ec9e29f8ad56f56d2
Alif Faishol <alifaishol@gmail.com>
2024-12-14 17:40:31 +0700

    add drizzle orm

---
+++ drizzle/0000_natural_cannonball.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "admins" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "admins_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+CREATE TABLE "conversations" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"service_id" uuid,
+	"service_state" json,
+	"user_id" uuid NOT NULL,
+	"messages" json
+);
+--> statement-breakpoint
+CREATE TABLE "services" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"workflow" json
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,41 @@
+import { relations } from "drizzle-orm";
+import { pgTable, uuid, varchar, json } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  username: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+});
+
+export const adminsTable = pgTable("admins", {
+  id: uuid().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  username: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+})
+
+export const servicesTable = pgTable("services", {
+  id: uuid().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  workflow: json(),
+});
+
+export const conversationsTable = pgTable("conversations", {
+  id: uuid().primaryKey(),
+  service_id: uuid(),
+  service_state: json(),
+  user_id: uuid().notNull(),
+  messages: json(),
+})
+
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  conversations: many(conversationsTable)
+}))
+
+export const conversationsRelations = relations(conversationsTable, ({ one }) => ({
+  service: one(servicesTable, {
+    fields: [conversationsTable.service_id],
+    references: [servicesTable.id]
+  })
+}))

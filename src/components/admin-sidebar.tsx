cf22a941b88e06b82d539f6c59ee946a270c696e
Alif Faishol <alifaishol@gmail.com>
2024-12-15 19:44:24 +0700

    placeholder workflow page

--- src/components/admin-sidebar.tsx
+++ src/components/admin-sidebar.tsx
@@ -1,10 +1,19 @@
-import { BookOpenCheckIcon, WorkflowIcon } from "lucide-react";
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { BookOpenCheckIcon, LandmarkIcon, WorkflowIcon } from "lucide-react";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from "./ui/sidebar";
 import Link from "next/link";
 
 const AdminSidebar = () => {
   return (
     <Sidebar>
+      <SidebarHeader>
+        <div className="px-2 py-4 flex items-center gap-2 font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <LandmarkIcon className="size-4" />
+          </div>
+          BUREAUSTREAM
+        </div>
+        <SidebarSeparator />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Administrations</SidebarGroupLabel>
@@ -27,7 +36,7 @@ const AdminSidebar = () => {
             <SidebarMenu>
                 <SidebarMenuItem>
                   <SidebarMenuButton asChild>
-                    <Link href="#">
+                    <Link href="/admin/workflow">
                       <WorkflowIcon />
                       <span>Service Workflows</span>
                     </Link>
